c33431c2c0371fb195e760abd6c2b2b81af0f654
HSGamer <huynhqtienvtag@gmail.com>
2022-04-04 13:47:50 +0700

    updateLight if not surrounded

--- api/src/main/java/me/hsgamer/blockutil/api/BlockUtil.java
+++ api/src/main/java/me/hsgamer/blockutil/api/BlockUtil.java
@@ -3,6 +3,9 @@ package me.hsgamer.blockutil.api;
 import me.hsgamer.blockutil.abstraction.BlockHandler;
 import me.hsgamer.blockutil.fallback.FallbackBlockHandler;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 
 import java.util.LinkedHashMap;
 import java.util.Locale;
@@ -63,5 +66,26 @@ public final class BlockUtil {
 
     public static BlockHandler getHandler() {
         return HANDLER;
+    }
+
+    public static boolean isSurrounded(Block block) {
+        final Block east = block.getRelative(BlockFace.EAST);
+        final Block west = block.getRelative(BlockFace.WEST);
+        final Block south = block.getRelative(BlockFace.SOUTH);
+        final Block north = block.getRelative(BlockFace.NORTH);
+        final Block up = block.getRelative(BlockFace.UP);
+        final Block down = block.getRelative(BlockFace.DOWN);
+        return !east.getType().isTransparent()
+                && !west.getType().isTransparent()
+                && !up.getType().isTransparent()
+                && !down.getType().isTransparent()
+                && !south.getType().isTransparent()
+                && !north.getType().isTransparent();
+    }
+
+    public static void updateLight(Block block) {
+        if (block.getType() != Material.AIR && !isSurrounded(block)) {
+            getHandler().updateLight(block);
+        }
     }
 }
