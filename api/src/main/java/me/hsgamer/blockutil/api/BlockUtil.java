eca204b4998fda3db94add543a6223aa0f17d650
HSGamer <huynhqtienvtag@gmail.com>
2022-04-04 14:06:38 +0700

    add v1_15_R1

--- api/src/main/java/me/hsgamer/blockutil/api/BlockUtil.java
+++ api/src/main/java/me/hsgamer/blockutil/api/BlockUtil.java
@@ -23,6 +23,7 @@ public final class BlockUtil {
         registerWithVersion("v1_18_R1");
         registerWithVersion("v1_17_R1");
         registerWithVersion("v1_16_R3");
+        registerWithVersion("v1_15_R1");
 
         AtomicReference<BlockHandler> handler = new AtomicReference<>();
         HANDLERS.forEach((supplier, blockHandlerSupplier) -> {
