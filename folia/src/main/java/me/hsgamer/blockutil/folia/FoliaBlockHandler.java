52354427db980a810fdb3cb5bf43394e7691d9dd
HSGamer <huynhqtienvtag@gmail.com>
2023-04-03 18:03:31 +0700

    Prepare methods on Folia

--- folia/src/main/java/me/hsgamer/blockutil/folia/FoliaBlockHandler.java
+++ folia/src/main/java/me/hsgamer/blockutil/folia/FoliaBlockHandler.java
@@ -4,6 +4,7 @@ import com.cryptomorin.xseries.XBlock;
 import com.cryptomorin.xseries.XMaterial;
 import com.lewdev.probabilitylib.ProbabilityCollection;
 import me.hsgamer.blockutil.abstraction.BlockHandler;
+import me.hsgamer.blockutil.abstraction.BlockHandlerSettings;
 import me.hsgamer.blockutil.abstraction.BlockProcess;
 import me.hsgamer.hscore.bukkit.block.BukkitBlockAdapter;
 import me.hsgamer.hscore.minecraft.block.box.BlockBox;
@@ -11,32 +12,70 @@ import me.hsgamer.hscore.minecraft.block.iterator.PositionIterator;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
 public class FoliaBlockHandler implements BlockHandler {
+    private final int blocksPerTick = Math.max(1, BlockHandlerSettings.BLOCKS_PER_TICK.get());
+    private final long blockDelay = Math.max(0, BlockHandlerSettings.BLOCK_DELAY.get());
     private final Plugin plugin;
 
     public FoliaBlockHandler(Plugin plugin) {
         this.plugin = plugin;
     }
 
+    // TODO: Integrate with Folia
+    private BlockProcess setBlocks(World world, PositionIterator iterator, Supplier<XMaterial> materialSupplier) {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        BukkitTask task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < blocksPerTick; i++) {
+                    if (iterator.hasNext()) {
+                        Block block = BukkitBlockAdapter.adapt(world, iterator.next()).getBlock();
+                        XBlock.setType(block, materialSupplier.get(), false);
+                    } else {
+                        cancel();
+                        future.complete(null);
+                        break;
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, blockDelay, blockDelay);
+        return new BlockProcess() {
+            @Override
+            public boolean isDone() {
+                return future.isDone();
+            }
+
+            @Override
+            public void cancel() {
+                task.cancel();
+            }
+        };
+    }
+
     @Override
     public BlockProcess setRandomBlocks(World world, PositionIterator iterator, ProbabilityCollection<XMaterial> probabilityCollection) {
-        return null;
+        return setBlocks(world, iterator, probabilityCollection::get);
     }
 
     @Override
     public BlockProcess setRandomBlocks(World world, BlockBox blockBox, ProbabilityCollection<XMaterial> probabilityCollection) {
-        return null;
+        return setRandomBlocks(world, BlockHandler.iterator(blockBox), probabilityCollection);
     }
 
     @Override
     public BlockProcess setBlocks(World world, PositionIterator iterator, XMaterial material) {
-        return null;
+        return setBlocks(world, iterator, () -> material);
     }
 
     @Override
     public BlockProcess setBlocks(World world, BlockBox blockBox, XMaterial material) {
-        return null;
+        return setBlocks(world, BlockHandler.iterator(blockBox), material);
     }
 
     @Override
